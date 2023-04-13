72a4d9775b78fd4780143e4ec1cb4ff79c1ab1af
huangweizhe <naruto60913@gmail.com>
2023-04-13 22:10:34 +0800

    切換fragment

--- app/src/main/java/com/example/foodrecipes/MainActivity.kt
+++ app/src/main/java/com/example/foodrecipes/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.foodrecipes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.foodrecipes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var databinding : ActivityMainBinding
+    private lateinit var navController : NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        databinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment,
+                R.id.favoriteRecipesFragment,
+                R.id.foodjokeFragment
+            )
+        )
+        setSupportActionBar(databinding.toolbar)
+        databinding.bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() ||  super.onSupportNavigateUp()
     }
 }
