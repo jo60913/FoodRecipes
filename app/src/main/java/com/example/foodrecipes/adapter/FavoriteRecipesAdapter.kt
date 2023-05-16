d3c8c88c21509d4c2868e89bbcd77cd8dc674fd2
huangweizhe <naruto60913@gmail.com>
2023-05-16 21:02:09 +0800

    在我的最愛長按後會出現刪除提示

--- app/src/main/java/com/example/foodrecipes/adapter/FavoriteRecipesAdapter.kt
+++ app/src/main/java/com/example/foodrecipes/adapter/FavoriteRecipesAdapter.kt
@@ -1,16 +1,23 @@
 package com.example.foodrecipes.adapter
 
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
+import android.view.*
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.foodrecipes.R
 import com.example.foodrecipes.data.database.entity.FavoritesEntity
 import com.example.foodrecipes.databinding.FavoriteRecipesRowLayoutBinding
+import com.example.foodrecipes.ui.fragment.main.FavoriteRecipesFragmentDirections
 import com.example.foodrecipes.util.RecipesDiffUtil
 
-class FavoriteRecipesAdapter : RecyclerView.Adapter<FavoriteRecipesAdapter.MyViewHolder>() {
+class FavoriteRecipesAdapter(
+    private val requireActivity: FragmentActivity
+) : RecyclerView.Adapter<FavoriteRecipesAdapter.MyViewHolder>(), ActionMode.Callback {
     private var favoriteRecipesList = emptyList<FavoritesEntity>()
+
     class MyViewHolder(private val binding: FavoriteRecipesRowLayoutBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
@@ -36,15 +43,49 @@ class FavoriteRecipesAdapter : RecyclerView.Adapter<FavoriteRecipesAdapter.MyVie
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         var recipes = favoriteRecipesList[position]
         holder.bind(recipes)
+        holder.itemView.setOnClickListener {
+            val action =
+                FavoriteRecipesFragmentDirections.actionFavoriteRecipesFragmentToDetailActivity(
+                    recipes.result
+                )
+            it.findNavController().navigate(action)
+        }
+
+        holder.itemView.setOnLongClickListener {    //長按時觸發
+            requireActivity.startActionMode(this)
+            true
+        }
     }
 
     override fun getItemCount() = favoriteRecipesList.size
 
-    fun setData(newFavoriteRecipes: List<FavoritesEntity>){
+    fun setData(newFavoriteRecipes: List<FavoritesEntity>) {
         val favoriteRecipesDiffUtil =
             RecipesDiffUtil(this.favoriteRecipesList, newFavoriteRecipes)
         val diffUtilResult = DiffUtil.calculateDiff(favoriteRecipesDiffUtil)
         this.favoriteRecipesList = newFavoriteRecipes
         diffUtilResult.dispatchUpdatesTo(this)
+    }
+
+    override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+        mode?.menuInflater?.inflate(R.menu.favorites_contextual_menu, menu)
+        applyStatueBarColor(R.color.contextStatusBarColor)
+        return true
+    }
+
+    override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?) = true
+
+    override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?) = true
+
+    override fun onDestroyActionMode(mode: ActionMode?) {
+        applyStatueBarColor(R.color.statusBarColor)
+    }
+
+    /***
+     * 改變狀態欄的顏色
+     */
+    private fun applyStatueBarColor(color: Int) {
+        requireActivity.window.statusBarColor =
+            ContextCompat.getColor(requireActivity,color)
     }
 }
