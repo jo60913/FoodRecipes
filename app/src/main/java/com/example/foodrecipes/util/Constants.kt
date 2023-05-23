ac71bd38c6d9b88e659ad9849d8837f648d779b3
huangweizhe <naruto60913@gmail.com>
2023-05-23 21:44:41 +0800

    修正api傳遞參數

--- app/src/main/java/com/example/foodrecipes/util/Constants.kt
+++ app/src/main/java/com/example/foodrecipes/util/Constants.kt
@@ -11,7 +11,7 @@ class Constants {
         const val QUERY_API_KEY = "apiKey"
         const val QUERY_TYPE = "type"
         const val QUERY_DIET = "diet"
-        const val QUERY_ADD_RECIPE_INFORMATION = "addRecipInformation"
+        const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
         const val QUERY_FILL_INGREDIENTS = "fillIngredients"
 
         //Room
