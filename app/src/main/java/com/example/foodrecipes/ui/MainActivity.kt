812d0b585451aa7fae0dcf98ea359c1534438ed5
huangweizhe <naruto60913@gmail.com>
2023-06-01 20:53:37 +0800

    設置firebase

--- app/src/main/java/com/example/foodrecipes/ui/MainActivity.kt
+++ app/src/main/java/com/example/foodrecipes/ui/MainActivity.kt
@@ -10,16 +10,19 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.foodrecipes.R
 import com.example.foodrecipes.databinding.ActivityMainBinding
+import com.google.firebase.analytics.FirebaseAnalytics
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var databinding : ActivityMainBinding
     private lateinit var navController : NavController
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         databinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         navController = findNavController(R.id.navHostFragment)
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.recipesFragment,
